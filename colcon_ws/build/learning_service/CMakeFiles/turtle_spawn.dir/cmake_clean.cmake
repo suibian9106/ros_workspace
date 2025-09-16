87c79060bba8fcf101df5562a45978ddf2901e0a
suibian9106 <330657991@qq.com>
2025-09-16 21:56:30 +0800

    learning_service update

---
+++ colcon_ws/build/learning_service/CMakeFiles/turtle_spawn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtle_spawn.dir/src/turtle_spawn.cpp.o"
+  "turtle_spawn"
+  "turtle_spawn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtle_spawn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
